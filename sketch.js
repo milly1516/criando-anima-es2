284b04867ce0e48d13ead0c80843fb0f8bbd69b8
milly1516 <149428556+milly1516@users.noreply.github.com>
2024-03-15 08:50:26 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,41 @@
+let cor;
+let posicaoHorizontal; // x
+let posicaoVertical; // y
+
 function setup() {
-    createCanvas(400, 400);
+  createCanvas(400, 400);
+   background(color(10,0,0));
+  cor = color(random(0,155), random(0,155), random(0,155));
+  posicaoHorizontal = 300;
+  posicaoVertical = 300;
+}
+
+
+function draw() {
+ 
+  fill(cor);
+  circle(posicaoHorizontal,posicaoVertical,10);
+ 
+ 
+ 
+  if (mouseX < posicaoHorizontal){
+    posicaoHorizontal =  posicaoHorizontal - 1;
   }
-  
-  function draw() {
-    background(220);
+ 
+  if (mouseX > posicaoHorizontal){
+    posicaoHorizontal =  posicaoHorizontal + 1;
   }
+ 
+  if (mouseY < posicaoVertical){
+    posicaoVertical--;
+  }
+   
+  if (mouseY > posicaoVertical){
+    posicaoVertical++;
+  }
+ 
+  if (mouseIsPressed){
+    cor = color(random(0,265), random(0,265), random(0,265), random(0,160));
+  }
+   
+}
